7e6f79d1ac21072c5e3d78e1fe34ec736a932f9f
igor-teixeira-souza <159443928+igor-teixeira-souza@users.noreply.github.com>
2025-03-18 13:53:47 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+//Configurar Back End
+//Criar duas variaveis - Consumir o framework express e instancia
+const express = require('express');
+const app = express();
+
+//Rota para os arquivos estaticos (public)
+app.use(express.static('public'));
+
+//Qual porta irá rodar
+app.listen(3000, () => {
+    console.log(`Servidor rodando no link http://localhost:3000`)
+})
